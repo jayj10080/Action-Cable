b361ba5184ecdb1cd7ef12c6f76923b5d94ec0e4
jay johnson <jayj10080@gmail.com>
2018-03-14 15:51:01 +0000

    Adjusted production.rb, routes, created procfile.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  require 'sidekiq/web'
   root 'rooms#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   mount ActionCable.server => '/cable'
+  mount Sidekiq::Web => '/sidekiq'
 end
